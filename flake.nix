bd2cf39ab2f9b16c5b352d3104eed9c61dedf5b6
Petar Kirov <petar.p.kirov@gmail.com>
2023-10-20 14:46:46 +0300

    wip: build(nix): Add `lido-keys-api` package

--- flake.nix
+++ flake.nix
@@ -17,8 +17,24 @@
         nodejs = pkgs.nodejs_20;
         yarn = pkgs.yarn.override {inherit nodejs;};
         defaultPackages = [nodejs yarn];
+        lido-keys-api = pkgs.callPackage ./yarn-project.nix {inherit nodejs;} {
+          src = pkgs.lib.cleanSource ./.;
+          overrideAttrs = super: {
+            name = "lido-keys-api";
+            buildInputs = super.buildInputs ++ [pkgs.python3];
+            buildPhase = ''
+              yarn postinstall
+              # yarn build
+            '';
+          };
+        };
       in {
+        packages = {
+          inherit lido-keys-api;
+          default = lido-keys-api;
+        };
         devShells.default = pkgs.mkShellNoCC {packages = defaultPackages;};
+        devShells.full = pkgs.mkShellNoCC {packages = defaultPackages ++ [lido-keys-api];};
       };
     };
 }
